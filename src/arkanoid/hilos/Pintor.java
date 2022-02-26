8326ff795c166cedf879e88fe0585c97cde7f3be
Litriboxer <litriboxer@hotmail.com>
2022-02-26 05:52:54 +0100

    cambios de Mar

--- src/arkanoid/hilos/Pintor.java
+++ src/arkanoid/hilos/Pintor.java
@@ -4,25 +4,26 @@ import arkanoid.ventanas.VentanaArkanoid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
 @Data
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode(callSuper=false)
 @AllArgsConstructor
 public class Pintor extends Thread {
-
+	
 	private VentanaArkanoid ventanaArkanoid;
-
+	
+	
 	@Override
 	public void run() {
-		while (true) {
+		while(true) {
 			getVentanaArkanoid().repaint();
 			try {
 				Thread.sleep(0);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-
+			
 		}
 	}
+	
 
 }
